9a0b6fa54dd08124de217f8c8a712ef8e5c86854
angellerina <angellerinac@gmail.com>
2021-02-24 14:35:25 -0500

    Initial FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,82 @@
+import React from "react";
+//Page Components
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
+            animi debitis! Deserunt itaque labore, sunt enim eveniet ratione
+            consectetur alias, fugit dolores ad perferendis porro culpa tenetur?
+            Distinctio, veritatis!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+            dignissimos ratione blanditiis veniam et nisi temporibus minus
+            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+            atque ab modi nihil aspernatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
+            animi debitis! Deserunt itaque labore, sunt enim eveniet ratione
+            consectetur alias, fugit dolores ad perferendis porro culpa tenetur?
+            Distinctio, veritatis!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+            dignissimos ratione blanditiis veniam et nisi temporibus minus
+            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+            atque ab modi nihil aspernatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
+            animi debitis! Deserunt itaque labore, sunt enim eveniet ratione
+            consectetur alias, fugit dolores ad perferendis porro culpa tenetur?
+            Distinctio, veritatis!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+            dignissimos ratione blanditiis veniam et nisi temporibus minus
+            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+            atque ab modi nihil aspernatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products Do You Offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
+            animi debitis! Deserunt itaque labore, sunt enim eveniet ratione
+            consectetur alias, fugit dolores ad perferendis porro culpa tenetur?
+            Distinctio, veritatis!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+            dignissimos ratione blanditiis veniam et nisi temporibus minus
+            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+            atque ab modi nihil aspernatur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
