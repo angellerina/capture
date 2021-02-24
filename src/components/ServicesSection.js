53dad6bba81ce56b73a0397479de510b13e76527
angellerina <angellerinac@gmail.com>
2021-02-24 18:47:59 -0500

    Styling FAQ Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,73 +1,86 @@
 import React from "react";
+// Import Icons
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+// Styles
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
-          HIgh <span>quality</span> services
+          High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="clock" />
               <h3>Efficient</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id
-              dolore aliquam tempore excepturi itaque facilis amet ad ratione
-              tenetur sequi, vel iure sit repellendus distinctio mollitia ut
-              dolorum quae! Nam!
-            </p>
-          </div>
-          <div className="card">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="teamwork" />
               <h3>Teamwork</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id
-              dolore aliquam tempore excepturi itaque facilis amet ad ratione
-              tenetur sequi, vel iure sit repellendus distinctio mollitia ut
-              dolorum quae! Nam!
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={diaphragm} alt="diaphragm" />
-            <h3>Diaphragm</h3>
-          </div>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id dolore
-            aliquam tempore excepturi itaque facilis amet ad ratione tenetur
-            sequi, vel iure sit repellendus distinctio mollitia ut dolorum quae!
-            Nam!
-          </p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={money} alt="money" />
-            <h3>Affordable</h3>
-          </div>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id dolore
-            aliquam tempore excepturi itaque facilis amet ad ratione tenetur
-            sequi, vel iure sit repellendus distinctio mollitia ut dolorum quae!
-            Nam!
-          </p>
-        </div>
-      </div>
-      <div className="image">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. !</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={money} alt="money" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="home" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+  }
+  h3 {
+    margin-left: 1rem;
+    background: #fff;
+    color: #000;
+    padding: 1rem;
+  }
+`;
 
 export default ServicesSection;
