113b09563557e644372ba13c97d43acb90bc8c51
angellerina <angellerinac@gmail.com>
2021-02-24 20:01:34 -0500

    Styled Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// import Styles
 import GlobalStyle from "./components/GlobalStyle";
+// import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
