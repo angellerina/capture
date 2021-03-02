789b78d6e1e5c950757ff032a9bfec1b7ac1d1a8
angellerina <angellerinac@gmail.com>
2021-03-02 18:52:39 -0500

    'OurWork' SVG animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,7 @@ import { About, Description, Image, Hide } from "../styles";
 // FramerMotion
 import { motion } from "framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   // Animations
@@ -33,6 +34,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
