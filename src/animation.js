cdccea23e355496481e53a44c18874f7b7ab91b3
angellerina <angellerinac@gmail.com>
2021-03-03 17:41:25 -0500

    Scroll Animation

--- src/animation.js
+++ src/animation.js
@@ -48,3 +48,14 @@ export const photoAnim = {
     },
   },
 };
+
+export const scrollReveal = {
+  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,12 +3,19 @@ import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
-  const [faqToggle, setFaqToggle] = useState(false);
+  const [element, controls] = useScroll();
 
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
