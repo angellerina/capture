48084f4fcb1b1424d7657b0b6a3890ad6587ae0d
angellerina <angellerinac@gmail.com>
2021-03-02 19:05:36 -0500

    Toggle Reusable Component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,66 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
+  const [faqToggle, setFaqToggle] = useState(false);
+
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
-            animi debitis!
-          </p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error
+              eius animi debitis!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              ipsam fuga ut ipsum, aperiam ipsa aliquid quaerat minus dolorum
+              officia?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. E</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. E</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>
-            Deserunt itaque labore, sunt enim eveniet ratione consectetur alias,
-            fugit dolores ad perferendis porro culpa tenetur? Distinctio,
-            veritatis!
-          </p>
-          <p>
-            Hic dignissimos ratione blanditiis veniam et nisi temporibus minus
-            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
-            atque ab modi nihil aspernatur?
-          </p>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>
+              Deserunt itaque labore, sunt enim eveniet ratione consectetur
+              alias, fugit dolores ad perferendis porro culpa tenetur?
+              Distinctio, veritatis!
+            </p>
+            <p>
+              Hic dignissimos ratione blanditiis veniam et nisi temporibus minus
+              recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+              atque ab modi nihil aspernatur?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products Do You Offer?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eius
-            animi debitis! Deserunt itaque labore, sunt enim eveniet ratione
-            consectetur alias, fugit dolores ad perferendis porro culpa tenetur?
-            Distinctio, veritatis!
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
-            dignissimos ratione blanditiis veniam et nisi temporibus minus
-            recusandae obcaecati iste amet, non adipisci commodi voluptatum,
-            atque ab modi nihil aspernatur?
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>What Products Do You Offer?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error
+              eius animi debitis! Deserunt itaque labore, sunt enim eveniet
+              ratione consectetur alias, fugit dolores ad perferendis porro
+              culpa tenetur? Distinctio, veritatis!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+              dignissimos ratione blanditiis veniam et nisi temporibus minus
+              recusandae obcaecati iste amet, non adipisci commodi voluptatum,
+              atque ab modi nihil aspernatur?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
     </Faq>
   );
 };
